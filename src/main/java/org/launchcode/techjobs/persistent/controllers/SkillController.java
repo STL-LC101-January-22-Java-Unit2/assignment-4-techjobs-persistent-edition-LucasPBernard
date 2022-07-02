2742e9391e3fc7f9e3551aa5bbbf7c961a0f781d
LucasPBernard <lucas.p.bernard@gmail.com>
2022-07-02 05:01:36 -0500

    controllers

--- src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -3,24 +3,20 @@ package org.launchcode.techjobs.persistent.controllers;
 import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 import java.util.Optional;
 
-@Controller
-@RequestMapping("employers")
-public class EmployerController {
-
+public class SkillController {
     @Autowired
     private EmployerRepository employerRepository;
 
-    //Add an index method that responds at /employers with a list of all employers in the database.
-    // This method should use the template employers/index. To figure out the name of the model attribute
-    // you should use to pass employers into the view, review this template.
     @GetMapping("employers")
     public String index(Model model, Employer employers) {
         model.addAttribute("employers",employers);
@@ -37,7 +33,7 @@ public class EmployerController {
 
     @PostMapping("add")
     public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
-                                    Errors errors, Model model) {
+                                         Errors errors, Model model) {
 
         if (errors.hasErrors()) {
             return "employers/add";
