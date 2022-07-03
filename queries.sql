a7257b21b1d8496e733966f62fc79fbf520a4ec4
LucasPBernard <lucas.p.bernard@gmail.com>
2022-07-03 04:18:03 -0500

    part 4

--- queries.sql
+++ queries.sql
@@ -7,3 +7,12 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
 DROP TABLE job;
 -- Part 4: Test it with SQL
+SELECT * FROM skill
+INNER JOIN job_skills ON (skill.id = job_skills.skills_id)
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
+
+SELECT\\s+\\*\\s+FROM\\s+skill" +
+"\\s*(LEFT|INNER)?\\s+JOIN\\s+job_skills\\s+ON\\s+(skill.id\\s+=\\s+job_skills.skills_id|job_skills.skills_id\\s+=\\s+skill.id)" +
+"(\\s*WHERE\\s+job_skills.jobs_id\\s+IS\\s+NOT\\s+NULL)?" +
+"\\s*ORDER\\s+BY\\s+name\\s+ASC;
--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -2,7 +2,9 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +24,9 @@ public class HomeController {
     @Autowired
     public EmployerRepository employerRepository;
 
+    @Autowired
+    public SkillRepository skillRepository;
+
     @RequestMapping("")
     public String index(Model model) {
 
@@ -35,6 +40,7 @@ public class HomeController {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
         model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         return "add";
     }
 
@@ -50,6 +56,10 @@ public class HomeController {
         Optional<Employer> employerToEdit = employerRepository.findById(employerId);
         Employer employer = employerToEdit.get();
         employer.setName(name);
+
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills(skillObjs);
+        
         return "redirect:";
     }
 
