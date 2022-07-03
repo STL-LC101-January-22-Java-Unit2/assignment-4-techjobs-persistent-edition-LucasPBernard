f187d542ab5f6ef20c13193715014904e39e425e
LucasPBernard <lucas.p.bernard@gmail.com>
2022-07-03 02:27:29 -0500

    part 3

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 -- Part 1: Test it with SQL
-SELECT * FROM techjobs.job;
+-- At this point, you will have one table, job. In queries.sql under “Part 1”, list the columns and their data types in the table as a SQL comment.
+-- id, employer, name, skills; int, Employer, String, String
 -- Part 2: Test it with SQL
-
+-- write a query to list the names of the employers in St. Louis City. Do NOT specify an ordering for the query results.
+SELECT name FROM employer WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
-
+DROP TABLE job;
 -- Part 4: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,13 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
     public Employer(){}
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private  List<Job> jobs = new ArrayList<>();
 
     @NotBlank(message = "Location is required")
     @Size(min = 3, max = 50, message = "Location must be between 3 and 50 characters")
